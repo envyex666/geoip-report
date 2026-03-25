622ef76ebe7af62a4cba18e3549171108fb32f82
envy <@env>
2026-03-25 10:15:58 +0300

    add server.py

---
+++ server.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, Request
+import geoip2.database
+
+app = FastAPI()
+
+@app.post("/client-data")
+def get_ip(request: Request):
+    client_host = request.client.host
+
+    with geoip2.database.Reader('/Users/envy/all-prac/stoledata/db/GeoLite2-Country.mmdb') as reader:
+         response = reader.country(client_host)
+        print('IP:', client_host, '\nCountry_Code:', response.country.iso_code, '\nCountry_name:', response.country.name)
+    return {"status": "OK"}
