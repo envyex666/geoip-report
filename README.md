7e19fea12d0bc33b39971544f047d4b87b681add
envy <@env>
2026-03-27 12:33:41 +0300

    1.3v

--- README.md
+++ README.md
@@ -1,6 +1,29 @@
-# App for making geoip reports
+# GEOIP REPORTS
+## App for making geoip reports from requests by clients
 
 ## QUICK START
+
+### add your token in .env
 ```bash
-cd scripts && chmod +x quick_start.sh && ./quick_start.sh
+echo "SECRET_TOKEN=secret_example" > .env
+```
+### make python virtual enviroments
+```bash
+python3 -m venv venv
+```
+### activate your venv
+```bash
+source venv/bin/activate
+```
+### install requirements
+```bash
+pip install -r requirements/requirements.txt
+```
+### go to src dir and start your server
+```bash
+cd src && uvicorn server:app --host 0.0.0.0 --port 8081
+```
+## Request example
+```bash
+curl -X POST "http://SERVER_IP:SERVER_PORT/report"   -H "Authorization: YOUR_TOKEN"
 ```
--- src/server.py
+++ src/server.py
@@ -1,14 +1,32 @@
+from typing import Annotated
 import geoip2.database
 import uvicorn
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+import os
+from dotenv import load_dotenv
 
 app = FastAPI()
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '../.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+secret_token = os.environ.get('SECRET_TOKEN')
+async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
+    if token != secret_token:
+      raise(HTTPException(status_code=401))
+    else:
+      return token
+
 @app.post("/report")
-def get_ip(request: Request):
+async def get_ip(request: Request, token: Annotated[str, Depends(oauth2_scheme)]):
+    await read_items(token)
     client_host = request.client.host
 
-    with geoip2.database.Reader("../Maxmind_CountryDB/GeoLite2-Country.mmdb") as reader:
+    with geoip2.database.Reader("db/GeoLite2-Country.mmdb") as reader:
         response = reader.country(client_host)
         print(
             "report_recieved ",
@@ -17,7 +35,7 @@ def get_ip(request: Request):
             "Country_Code:",
             response.country.iso_code,
             "Country_name:",
-            response.country.name,
+            response.country.name
         )
 
     return {"status": "OK"}
